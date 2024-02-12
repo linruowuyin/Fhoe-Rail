d1908e1b114da6ee9af8f01ef3dea6f57795c53c
linruowuyin <63692049+linruowuyin@users.noreply.github.com>
2024-02-12 16:09:41 +0800

    Add files via upload

--- utils/calculated（改按键）.py
+++ utils/calculated（改按键）.py
@@ -1,4 +1,21 @@
 import time
+import cv2 as cv
+import numpy as np
+import pyautogui
+import win32api
+import win32con
+import win32gui
+import random
+from datetime import datetime
+from PIL import ImageGrab
+from pynput.keyboard import Controller as KeyboardController
+
+from .config import read_json_file, CONFIG_FILE_NAME
+from .exceptions import Exception
+from .log import log
+from .mini_asu import ASU
+
+import time
 
 import cv2 as cv
 import numpy as np
@@ -6,55 +23,37 @@ import pyautogui
 import win32api
 import win32con
 import win32gui
-import pyautogui
 import random
 from datetime import datetime
 from PIL import ImageGrab
 from pynput.keyboard import Controller as KeyboardController
-from pynput.mouse import Listener as MouseListener
 
 from .config import read_json_file, CONFIG_FILE_NAME
 from .exceptions import Exception
 from .log import log
+from .mini_asu import ASU
+
 class Calculated:
     def __init__(self):
         self.CONFIG = read_json_file(CONFIG_FILE_NAME)
         self.keyboard = KeyboardController()
-
-    def on_move(self, x, y):
-        pass
-
-    def wait_for_mouse_stop(self, initial_pos):
-        current_pos = win32api.GetCursorPos()
-        while current_pos != initial_pos:
-            time.sleep(0.1)
-            initial_pos = current_pos
-            current_pos = win32api.GetCursorPos()
-
-    def move_to_and_click(self, x, y):
-        initial_pos = win32api.GetCursorPos()
-        with MouseListener(on_move=self.on_move) as listener:
-            win32api.SetCursorPos((x, y))
-            self.wait_for_mouse_stop(initial_pos)
-            current_pos = win32api.GetCursorPos()
-            if current_pos == (x, y):
-                pyautogui.mouseDown(x, y, button='left')
-                time.sleep(0.4)
-                pyautogui.mouseUp(x, y, button='left')
-            else:
-                win32api.SetCursorPos((x, y))
-                pyautogui.mouseDown(x, y, button='left')
-                time.sleep(0.4)
-                pyautogui.mouseUp(x, y, button='left')
+        self.ASU = ASU()
 
     def click(self, points):
+        """
+        说明：
+            点击坐标
+        参数：
+            :param points: 坐标
+        """
         x, y = int(points[0]), int(points[1])
-        screen_width, screen_height = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
-        x = max(0, min(x, screen_width - 1))
-        y = max(0, min(y, screen_height - 1))
-
-        # 确保鼠标点击位置不超出屏幕边界
-        self.move_to_and_click(x, y)
+        log.debug((x, y))
+        
+        # 移动鼠标并点击
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        time.sleep(0.5)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
     def relative_click(self, points):
         """
@@ -72,19 +71,51 @@ class Calculated:
         )
         log.info((x, y))
         log.debug((x, y))
+        
+        # Press Alt key
+        win32api.keybd_event(win32con.VK_MENU, 0, 0, 0)
+        
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-        time.sleep(0.5)
+        time.sleep(0.4)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+        win32api.keybd_event(win32con.VK_MENU, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+    def click_center(self):
+        """
+        点击游戏窗口中心位置
+        """
+        hwnd = win32gui.FindWindow("UnityWndClass", "崩坏：星穹铁道")
+        left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+        x = int((left + right) / 2)
+        y = int((top + bottom) / 2)
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        time.sleep(0.1)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
     def take_screenshot(self):
-        # 返回RGB图像
+        """
+        获取游戏窗口的屏幕截图
+        """
         hwnd = win32gui.FindWindow("UnityWndClass", "崩坏：星穹铁道")
         left, top, right, bottom = win32gui.GetWindowRect(hwnd)
-        picture = ImageGrab.grab((left, top, right, bottom), all_screens=True)
+
+        # 计算截图范围
+        screenshot_left = left
+        screenshot_top = top
+        screenshot_right = min(right, left + 1920)
+        screenshot_bottom = min(bottom, top + 1080)
+
+        # 如果游戏窗口的底部坐标高于 1080，则调整截图范围
+        if bottom > 1080:
+            screenshot_top = max(top, bottom - 1080)
+
+        # 获取游戏窗口截图
+        picture = ImageGrab.grab((screenshot_left, screenshot_top, screenshot_right, screenshot_bottom), all_screens=True)
         screenshot = np.array(picture)
         screenshot = cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)
-        return screenshot, left, top, right, bottom
+        return screenshot, screenshot_left, screenshot_top, screenshot_right, screenshot_bottom
 
     def scan_screenshot(self, prepared) -> dict:
         """
@@ -109,7 +140,6 @@ class Calculated:
         prepared_height, prepared_width, prepared_channels = shape
 
         x = int((mat_top + mat_top + prepared_width) / 2)
-
         y = int((mat_left + mat_left + prepared_height) / 2)
 
         return x, y
@@ -117,6 +147,7 @@ class Calculated:
     # flag为true一定要找到
     def click_target(self, target_path, threshold, flag=True):
         target = cv.imread(target_path)
+        start_time = time.time()
         while True:
             result = self.scan_screenshot(target)
             if result["max_val"] > threshold:
@@ -125,6 +156,19 @@ class Calculated:
                 return
             if not flag:
                 return
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 30:
+                return
+            
+    def click_target_with_alt(self, target_path, threshold, flag=True):
+        # 按下Alt键
+        win32api.keybd_event(win32con.VK_MENU, 0, 0, 0)
+        
+        # 调用click_target方法
+        self.click_target(target_path, threshold, flag)
+        
+        # 释放Alt键
+        win32api.keybd_event(win32con.VK_MENU, 0, win32con.KEYEVENTF_KEYUP, 0)
 
     def fighting(self):
         start_time = time.time()
@@ -138,12 +182,12 @@ class Calculated:
             warn_result = self.scan_screenshot(warn)
             if attack_result["max_val"] > 0.9:
                 points = self.calculated(attack_result, attack.shape)
-                self.click(points)
+                self.click_center()
                 break
             elif doubt_result["max_val"] > 0.9 or warn_result["max_val"] > 0.9:
                 log.info("識別到疑問或警告，等待怪物開戰")
                 points = self.calculated(attack_result, attack.shape)
-                self.click(points)
+                self.click_center()
                 time.sleep(5)
                 target = cv.imread("./picture/finish_fighting.png")  # 識別是否已進入戰鬥，若已進入則跳出迴圈
                 result = self.scan_screenshot(target)
@@ -160,7 +204,7 @@ class Calculated:
                 result = self.scan_screenshot(target)
                 if result["max_val"] > 0.9:
                     points = self.calculated(result, target.shape)
-                    self.click(points)
+                    self.click_center()
                     log.info("开启自动战斗")
                     break
                 elif time.time() - start_time > 15:
@@ -173,19 +217,34 @@ class Calculated:
         target = cv.imread("./picture/finish_fighting.png")
         while True:
             result = self.scan_screenshot(target)
-            if result["max_val"] > 0.9:
+            if result["max_val"] > 0.92:
                 points = self.calculated(result, target.shape)
                 log.debug(points)
                 elapsed_time = time.time() - start_time
                 elapsed_minutes = int(elapsed_time // 60)
                 elapsed_seconds = elapsed_time % 60
-                formatted_time = f"{elapsed_minutes}分钟{elapsed_seconds:.0f}秒"
                 formatted_time = f"{elapsed_minutes}分钟{elapsed_seconds:.2f}秒"
                 current_system_time = time.localtime()
                 colored_message = (f"战斗完成,单场用时\033[1;92m『{formatted_time}』\033[0m")
                 log.info(colored_message)
+                match_details = f"匹配度: {result['max_val']:.2f} ({points[0]}, {points[1]})"
+                log.info(match_details)
+
+                self.rotate()
                 time.sleep(3)
                 break
+            
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 90:
+                self.click_target("./picture/auto.png", 0.97, False)  #超时尝试开启自动战斗
+                self.click_target("./picture/defeat.png", 0.96, False)  #战斗失败
+                self.click_target("./picture/map_4-2_point_3.png", 0.97, False)  #3D地图返回
+                self.click_target("./picture/orientation_close.png", 0.98, False)  #退出地图界面
+                if elapsed_time > 900:
+                    log.info("战斗超时")
+                    break
+                time.sleep(0.5)
+
 
     def fightE(self):
         start_time = time.time()
@@ -194,38 +253,38 @@ class Calculated:
         warn = cv.imread("./picture/warn.png")
         image_A = cv.imread("./picture/eat.png")  # 修改
         image_B = cv.imread("./picture/cancel.png")  # 修改
-
+        
         while True:
             log.info("识别中")
             attack_result = self.scan_screenshot(attack)
             doubt_result = self.scan_screenshot(doubt)
             warn_result = self.scan_screenshot(warn)
-
+            
             if attack_result["max_val"] > 0.9:
-                pyautogui.press('f')
+                pyautogui.press('e')
                 time.sleep(1)
                 start_time_A = time.time()
                 result_A = None
                 while result_A is None and time.time() - start_time_A < 3:
                     result_A = self.scan_screenshot(image_A)
-
+                    
                 if result_A is not None and result_A["max_val"] > 0.9:
                     while True:
                         result_B = self.scan_screenshot(image_B)
                         points_B = self.calculated(result_B, image_B.shape)
                         self.click(points_B)
                         if result_B is None or result_B["max_val"] < 0.9:  # 修改条件判断语句
-                            break
+                           break
                         else:
                             break
-
+                        
                 points = self.calculated(attack_result, attack.shape)
                 time.sleep(3)
-                self.click(points)
+                self.click_center()
                 break
             elif doubt_result["max_val"] > 0.9 or warn_result["max_val"] > 0.9:
                 log.info("識別到疑問或警告，等待怪物開戰")
-                self.click(points)
+                self.click_center()
                 time.sleep(3)
                 target = cv.imread("./picture/finish_fighting.png")  # 識別是否已進入戰鬥，若已進入則跳出迴圈
                 result = self.scan_screenshot(target)
@@ -242,7 +301,7 @@ class Calculated:
                 result = self.scan_screenshot(target)
                 if result["max_val"] > 0.9:
                     points = self.calculated(result, target.shape)
-                    self.click(points)
+                    self.click_center()
                     log.info("开启自动战斗")
                     break
                 elif time.time() - start_time > 15:
@@ -255,36 +314,48 @@ class Calculated:
         target = cv.imread("./picture/finish_fighting.png")
         while True:
             result = self.scan_screenshot(target)
-            if result["max_val"] > 0.9:
+            if result["max_val"] > 0.92:
                 points = self.calculated(result, target.shape)
                 log.debug(points)
                 elapsed_time = time.time() - start_time
                 elapsed_minutes = int(elapsed_time // 60)
                 elapsed_seconds = elapsed_time % 60
-                formatted_time = f"{elapsed_minutes}分钟{elapsed_seconds:.0f}秒"
                 formatted_time = f"{elapsed_minutes}分钟{elapsed_seconds:.2f}秒"
                 current_system_time = time.localtime()
                 colored_message = (f"战斗完成,单场用时\033[1;92m『{formatted_time}』\033[0m")
                 log.info(colored_message)
+                match_details = f"匹配度: {result['max_val']:.2f} ({points[0]}, {points[1]})"
+                log.info(match_details)
+
+                self.rotate()
                 time.sleep(3)
                 break
+            
+    def rotate(self):
+        if self.need_rotate:
+            self.keyboard.press('w')
+            self.keyboard.release('w')
+            time.sleep(0.7)
+            self.ASU.screen = self.take_screenshot()[0]
+            ang = self.ang - self.ASU.get_now_direc()
+            ang = (ang + 900) % 360 - 180
+            self.mouse_move(ang * 10.2)
 
-    def auto_map(self, map, old=True):
+    def auto_map(self, map, old=True, rotate=False):
+        self.ASU.screen = self.take_screenshot()[0]
+        self.ang = self.ASU.get_now_direc()
+        self.need_rotate = rotate
         map_data = (
             read_json_file(f"map\\old\\{map}.json")
             if old
             else read_json_file(f"map\\{map}.json")
         )
         map_filename = map
-        # 开始寻路
-        log.info("开始寻路")
         for map_index, map in enumerate(map_data["map"]):
             log.info(f"执行{map_filename}文件:{map_index + 1}/{len(map_data['map'])} {map}")
             key = list(map.keys())[0]
             value = map[key]
-            if key == "f":
-                # 生成0.3到0.7之间的随机浮点数
-                key = "e"
+            if key == "e" or key == "space" or key == "r":  # 交换了 "e" 和 "f"
                 random_interval = random.uniform(0.3, 0.7)
                 num_repeats = int(value / random_interval)
                 for i in range(num_repeats):
@@ -313,15 +384,23 @@ class Calculated:
                     os.system("shutdown /s /f /t 0")
                 else:
                     log.info("锄地结束！")
-            elif key == "e":
+            elif key == "f":  # 这里改成了 "f"
                 if value == 1:  # 进战斗
-                    self.fightE()
+                    self.fightE()  # 这里调用了原来的 fightE 方法
+            elif key == "Esc":
+                if value == 1:  # 执行一次Esc键的按下和释放操作
+                    win32api.keybd_event(win32con.VK_ESCAPE, 0, 0, 0) 
+                    time.sleep(0.1)  
+                    win32api.keybd_event(win32con.VK_ESCAPE, 0, win32con.KEYEVENTF_KEYUP, 0)
+                else:
+                    raise Exception((f"map数据错误, Esc参数只能为1:{map_filename}", map))
             else:
                 self.keyboard.press(key)
                 start_time = time.perf_counter()
                 while time.perf_counter() - start_time < value:
                     pass
                 self.keyboard.release(key)
+
 
     def mouse_move(self, x):
         scaling = 1.0
@@ -368,12 +447,20 @@ class Calculated:
 
     def is_blackscreen(self, threshold=25):
         screenshot = cv.cvtColor(self.take_screenshot()[0], cv.COLOR_BGR2GRAY)
-        
+
         if cv.mean(screenshot)[0] > threshold:  # 如果平均像素值大于阈值
             target = cv.imread("./picture/finish_fighting.png")
-            while True:
+            alt_target = cv.imread("./picture/finish_fighting2.png")
+            attempts = 0
+            max_attempts_ff1 = 3
+            while attempts < max_attempts_ff1:
                 result = self.scan_screenshot(target)
-                if result["max_val"] > 0.9:
+                alt_result = self.scan_screenshot(alt_target)
+                if result and result["max_val"] > 0.9:
                     return False  # 如果匹配度大于0.9，表示不是黑屏，返回False
+                elif alt_result and alt_result["max_val"] > 0.92:
+                    return False  # 如果匹配度大于0.92，表示不是黑屏，返回False
+                attempts += 1
+                time.sleep(2)  # 等待2秒再尝试匹配
 
-        return cv.mean(screenshot)[0] < threshold
+        return True  # 如果未匹配到指定的图像，返回True
