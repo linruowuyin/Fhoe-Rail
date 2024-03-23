1f8a14b8377cc9d853e3e258403e02e29551adde
linruowuyin <63692049+linruowuyin@users.noreply.github.com>
2024-03-23 15:18:16 +0800

    Add files via upload

--- 1点这里.bat
+++ 1点这里.bat
@@ -1,15 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM 设置当前目录为批处理文件所在目录
 CD /D "%~dp0"
-REM 设置窗口标题
 TITLE 【Fhoe-Rail】
 
-REM 调整控制台窗口大小
-mode con cols=85 lines=40
-
-REM 检查并获取管理员权限
 >nul 2>&1 REG.exe query "HKU\S-1-5-19" || (
     ECHO Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\Getadmin.vbs"
     ECHO UAC.ShellExecute "%~f0", "%1", "", "runas", 1 >> "%TEMP%\Getadmin.vbs"
@@ -40,41 +34,29 @@ echo   [2] 速启（默认1-1，无操作会自动选择）
 echo.
 echo   [3] 地图（选择具体地图）
 echo.
-echo   [4] 配置（配置后选地图）
+echo   [4] 录制
 echo.
-echo   [5] 录制
-echo.
-echo   [6] 测试（测图用,F8暂停）
+echo   [5] 测试（测图用,F8暂停）
 echo.
 
 REM 等待用户输入或超时
 choice /C 123456 /T 30 /D 2 /N >nul
 
 REM 处理用户输入
-if errorlevel 6 (
-    color 0C
+if errorlevel 5 (
     echo.
     echo   Tips：按F8暂停，暂停后按F8继续，按F9重新运行当前地图
     python -i -X utf8 Honkai_Star_Rail.py --dev
     echo.
     pause
     goto :end
-) else if errorlevel 5 (
-    color 0B
+) else if errorlevel 4 (
     echo.
     python -i -X utf8 utils/record.py
     echo.
     pause
     goto :end
-) else if errorlevel 4 (
-    color 0D
-    echo.
-    python -i -X utf8 Honkai_Star_Rail.py --config
-    echo.
-    pause
-    goto :end
 ) else if errorlevel 3 (
-    color 0E
     echo.
     echo   正在启动程序...
     echo.
@@ -83,7 +65,6 @@ if errorlevel 6 (
     pause
     goto :end
 ) else if errorlevel 2 (
-    color 0F
     echo.
     echo   正在极速启动...
     echo.
@@ -92,7 +73,6 @@ if errorlevel 6 (
     pause
     goto :end
 ) else (
-    color 0A
     echo.
     echo   正在检查并安装依赖...
     echo.
